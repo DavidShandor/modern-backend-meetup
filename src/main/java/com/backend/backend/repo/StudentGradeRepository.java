f138c4b1ca0e72e1918e61272d50e302db3018a4
DavidShandor <davidshandor15@gmail.com>
2024-01-31 10:13:46 +0200

    with one to many

---
+++ src/main/java/com/backend/backend/repo/StudentGradeRepository.java
@@ -0,0 +1,9 @@
+package com.backend.backend.repo;
+
+
+import com.backend.backend.model.StudentGrade;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentGradeRepository extends CrudRepository<StudentGrade,Long> {
+
+}
